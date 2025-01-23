86dadbc8d3fa492159e60977be21bab9099000f4
Kabir <kabir.gupta@deepmindz.co>
2025-01-23 23:38:32 +0530

    Added health route and cors middleware

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI, Response, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from search import search_images
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with specific domains for better security
+    allow_credentials=True,
+    allow_methods=["*"],  # Specify allowed HTTP methods
+    allow_headers=["*"],  # Specify allowed headers
+)
+
+
+@app.get("/")
+def ping():
+    return "API in Running!"
+
 
 @app.get("/find")
-def health(request: Request, response: Response):
+def search_image(request: Request, response: Response):
 
     query_params = request.query_params
     query = query_params.get("query","")
